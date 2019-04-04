7af0fe272dfd01f3ad220c1890424f9833b58539
dragonruchu <oracledownload@qq.com>
2019-04-04 21:47:41 +0800

    train rnn on mnist

---
+++ rnn_for_mnist.py
@@ -0,0 +1,187 @@
+'''
+Reference:
+https://github.com/L1aoXingyu/pytorch-beginner/blob/master/05-Recurrent%20Neural%20modelwork/recurrent_modelwork.py
+'''
+
+import torch
+from torch import nn, optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision import datasets
+
+from utils import progress_bar
+import os
+import math
+import matplotlib.pyplot as plt
+
+batch_size = 128
+learning_rate = 0.001
+num_epochs = 20
+best_acc = 0
+
+# Download data
+train_dataset = datasets.MNIST(root="./data", train=True, transform=transforms.ToTensor(), download=True)
+test_dataset = datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor(), download=True)
+
+# data loader
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+# Recurrent modelwork
+
+
+class Rnn(nn.Module):
+    def __init__(self, in_dim, hidden_dim, n_layer, n_class):
+        super(Rnn, self).__init__()
+        self.n_layer = n_layer
+        self.hidden_dim = hidden_dim
+        self.lstm = nn.LSTM(in_dim, hidden_dim, n_layer, batch_first=True)
+        self.classifier = nn.Linear(hidden_dim, n_class)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = out[:, -1, :]
+        out = self.classifier(out)
+        return out
+
+
+model = Rnn(28, 128, 2, 10)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = model.to(device)
+# loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+
+# start training
+def train(epoch):
+    print("Epoch %s " % (epoch))
+
+    model.train()
+    train_loss = 0.0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(train_loader):
+        # b, c, h, w = inputs.size()
+        # assert c == 1, 'channel must be 1'
+        # print(inputs.shape)
+        inputs = inputs.squeeze(1)
+        # print(inputs.shape)
+
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        progress_bar(batch_idx, len(train_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                     % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+
+def test():
+    global best_acc
+    model.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(test_loader):
+            inputs = inputs.squeeze(1)
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(test_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (test_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+    # Save checkpoint.
+    acc = 100. * correct / total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'model': model.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/ckpt.t7')
+        best_acc = acc
+
+
+for epoch in range(1, num_epochs + 1):
+    train(epoch)
+    test()
+
+
+def find_lr(init_value=1e-8, final_value=10., beta=0.98):
+    num = len(train_loader) - 1
+    mult = (final_value / init_value) ** (1 / num)
+    lr = init_value
+    optimizer.param_groups[0]['lr'] = lr
+
+    avg_loss = 0.
+    best_loss = 0.
+    batch_num = 0
+    losses = []
+    log_lrs = []
+
+    total = 0
+    correct = 0
+
+    for batch_idx, (inputs, targets) in enumerate(train_loader):
+        batch_num += 1
+
+        inputs = inputs.squeeze(1)
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+
+        # compute the smoothed loss
+        avg_loss = beta * avg_loss + (1 - beta) * loss.item()
+        smoothed_loss = avg_loss / (1 - beta ** batch_num)
+
+        # Stop if the loss is exploding
+        if batch_num > 1 and smoothed_loss > 4 * best_loss:
+            return log_lrs, losses
+
+        # Record the best loss
+        if smoothed_loss < best_loss or batch_num == 1:
+            best_loss = smoothed_loss
+
+        # Store the values
+        losses.append(smoothed_loss)
+        log_lrs.append(math.log10(lr))
+
+        loss.backward()
+        optimizer.step()
+
+        lr *= mult
+        optimizer.param_groups[0]['lr'] = lr
+
+        _, pred = outputs.max(1)
+        total += targets.size(0)
+        correct += pred.eq(targets).sum().item()
+        progress_bar(batch_idx, len(train_loader), "Loss: %.3f | Acc: %.3f%% (%d/%d)"
+                     % (avg_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+    return log_lrs, losses
+
+
+# print("Now is finding best lr:")
+# logs, losses = find_lr()
+# plt.plot(logs, losses)
+# plt.show()
